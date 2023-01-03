809cb79bffc766ea57ef3aa94001e09896730656
NersesArslan <108882259+NersesArslan@users.noreply.github.com>
2023-01-02 19:10:39 -0800

    figured out how to switch tabs, refactor code

--- src/index.js
+++ src/index.js
@@ -73,9 +73,7 @@ main.appendChild(container)
 
 
 
-
-
-    const header = document.createElement('h1')
+const header = document.createElement('h1')
     header.classList.add('header')
     header.textContent = 'Welcome to Pasolini\'s!'
     container.appendChild(header)
@@ -86,25 +84,35 @@ main.appendChild(container)
     paragraph.classList.add('main-text')
     container.appendChild(paragraph)
 
+function homePageRendering() {
+
+
+    const header = document.createElement('h1')
+    header.classList.add('header')
+    header.textContent = 'Welcome to Pasolini\'s!'
+    container.appendChild(header)
     
+    const paragraph = document.createElement('p')
+    paragraph.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mattis enim ut tellus elementum. Faucibus vitae aliquet nec ullamcorper sit amet risus. Neque convallis a cras semper auctor. Lacus viverra vitae congue eu consequat. Et malesuada fames ac turpis egestas integer. Aliquet porttitor lacus luctus accumsan tortor posuere ac ut. Proin nibh nisl condimentum id venenatis a condimentum. Nec dui nunc mattis enim ut tellus elementum. Sed egestas egestas fringilla phasellus faucibus scelerisque. Aenean sed adipiscing diam donec adipiscing tristique. Tortor id aliquet lectus proin nibh nisl condimentum id. Commodo odio aenean sed adipiscing diam donec. Elementum sagittis vitae et leo. Pulvinar neque laoreet suspendisse interdum consectetur. Ac tincidunt vitae semper quis lectus nulla at. Habitant morbi tristique senectus et netus et malesuada.'
+    paragraph.classList.add('main-text')
+    container.appendChild(paragraph)
 
 
+}
 
 
-
-
-// menuPageRendering(container);
-
- //testFunction(container)
-
- home.addEventListener('click', () => {
-    
+home.addEventListener('click', () => {
+    container.innerHTML = ''
     homePageRendering()
- })
+})
 
- menu.addEventListener('click', () => {
-    container.removeChild(header);
-    container.removeChild(paragraph);
 
-    menuPageRendering(container);
- })
+menu.addEventListener('click', () => {
+    container.innerHTML = ''
+    menuPageRendering(container)
+})
+
+contact.addEventListener('click', () => {
+    container.innerHTML = ''
+    testFunction(container)
+})
